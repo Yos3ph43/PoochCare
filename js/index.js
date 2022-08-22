3ec9c9039d6c2904f2cb4e01e3782614056cad58
Minh Luan <luan040399@gmail.com>
2022-08-22 10:32:48 +0700

    fix footer, done plan, add video popup

--- js/index.js
+++ js/index.js
@@ -1,9 +1,46 @@
-window.onscroll = function() {scrollFunction()};
+// navbar shrink
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "0px";    
+    document.getElementById("navbar").style.padding = "0px";
   } else {
-    document.getElementById("navbar").style.padding = "5px";    
+    document.getElementById("navbar").style.padding = "5px";
   }
 }
+
+// backtotop button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("movetop").style.display = "block";
+  } else {
+    document.getElementById("movetop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// video popup
+
+$(document).ready(function () {
+  $(".popup-with-zoom-anim").magnificPopup({
+    type: "inline",
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: "auto",
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: "my-mfp-zoom-in",
+  });
+});
