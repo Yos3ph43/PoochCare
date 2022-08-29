3470a7cedf5dafd7d4ab2e1fa52bea79c33eddd9
Minh Luan <luan040399@gmail.com>
2022-08-29 14:26:43 +0700

    reset master to merge

--- js/index.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "0px";    
-  } else {
-    document.getElementById("navbar").style.padding = "5px";    
-  }
-}
